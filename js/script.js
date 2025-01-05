1743cd4ab880719adfa1b93ecb80a3c28b1736b4
aswinzainuddin <azwinmuhammad4@gmail.com>
2025-01-05 21:31:14 +0700

    updating shopping cart section and search bar toggle

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const navbar = document.querySelector(".navbar");
 
 // when menu is clicked
-document. querySelector("#menu").onclick = () => {
+document.querySelector("#menu").onclick = () => {
     navbar.classList.toggle("active");
 }
 
@@ -16,11 +16,32 @@ document.addEventListener("click", function(e) {
 
 // search bar active
 const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#searchBox");
+const searchButton = document.querySelector("#searchButton");
 
 document.querySelector("#searchButton").onclick = (e) => {
     searchForm.classList.toggle("active");
     searchBox.focus();
     e.preventDefault();
 }
+// removing search bar active
+document.addEventListener("click", function(e) { 
+    if(!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove("active");
+    }
+});
+
+// shopping cart active 
+const shoppingCart = document.querySelector(".shopping-cart");
+// when it is clicked 
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+    shoppingCart.classList.toggle("active");
+    e.preventDefault();
+}
+// removing shopping cart toggle
+const sc = document.querySelector("#shopping-cart-button");
+document.addEventListener("click", function(e) { 
+    if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove("active");
+    }
+});
 
